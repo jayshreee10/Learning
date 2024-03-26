532ed4f7453bf61a17008480dbd35fb4bc8ae231
jayshreee10 <2001109280_ee_pmec.ac.in>
2024-03-26 23:14:56 +0530

    learning ASCII

--- JavaScript/Function/atoz.js
+++ JavaScript/Function/atoz.js
@@ -1,8 +1,11 @@
 // printing a to z
 
-
+function alphabets (){
   let start = "a".charCodeAt(0);
   let stop = "z".charCodeAt(0);
   for (i = start; i <= stop; i++) {
     console.log(String.fromCharCode(i));
   }
+
+}
+alphabets()
