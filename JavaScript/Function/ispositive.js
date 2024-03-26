7c84b84a66b0f9787d9779a68001e1eb2569106c
jayshreee10 <2001109280_ee_pmec.ac.in>
2024-03-26 19:03:46 +0530

    playing with function

---
+++ JavaScript/Function/ispositive.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Check if a number is Positive, Negative, or Zero
+
+const value = function isPositive(number) {
+  if (number >= 0) {
+    console.log(number + " is positive");
+    return true;
+  } else {
+      console.log (number + " is negative")
+      return false;
+  }
+};
+
+value(-7);
