b6c22036f56550cabe07b1eb7d9f6cee8541db16
jayshreee10 <2001109280_ee_pmec.ac.in>
2024-03-29 20:46:03 +0530

    solving palindrome problem

--- JavaScript/Function/validation.js
+++ JavaScript/Function/validation.js
@@ -11,4 +11,28 @@ let myArr = [100, 205, 396, 107, 829, 721, 950];
  step 6 :   print the array ;
  */
 
+
+ //soliving palindrome 
+//  word : level
+
+function palindrome (word){
+    let  wordLength = word.length 
+    let message = ""
+    for (i=0;i<= wordLength/2;i++){ 
+        // console.log(word[i]);
+        let palindromeWord = (wordLength  - 1 - i )
+        // console.log(  palindromeWord )
+        if( word[i] != word[palindromeWord] ){
+            message = "not a palindrome word"
+        }
+        else { message =  "is a palindrome word"}
+    }
+    console.log (message)
+   
+}
+palindrome ( "level")
+ 
+//another approach
+// madam
+
  
