50a725f8028d7e0107f0c736d7093a854ee4fdab
Jayshree sadangi <155508849+jayshreee10@users.noreply.github.com>
2024-03-30 12:04:35 +0530

    Add files via upload

---
+++ JavaScript/Function/interview.js
@@ -0,0 +1,51 @@
+// find duplicate elements in an array
+
+//find max/min in a given array
+
+//find second Largest value in array
+
+// find the sum of all elements in array
+
+// find even num sum or odd num sum numbers in array
+
+// find fictorial of a given number 
+
+//  find prime number
+
+// find vowels from string
+
+//fibonacci series
+
+// reverse a string
+
+//find palindrome
+
+//swap two variables without using the third
+
+//swap two variables without using the third
+
+//  find factor of a given integer
+
+//calculator
+
+//compare two Arrays are Equal or Not 
+
+// find intersection of two arrays
+
+//find union of two arrays in javascript
+
+//convert celsius to fahrenheit 
+
+// convert km to miles
+
+//convert first letter of string in uppercase
+
+// print star pattern in diamond shape
+
+//Check the No of Occurrence of Character in String
+
+//print the table of any user defined number
+
+//Check Armstrong Number in JavaScript
+
+
