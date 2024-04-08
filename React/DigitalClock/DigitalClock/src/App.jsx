88637e881d56dcc30785c76e2d3dba32869d138e
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-08 22:36:10 +0530

    Updated Ui

--- React/DigitalClock/DigitalClock/src/App.jsx
+++ React/DigitalClock/DigitalClock/src/App.jsx
@@ -1,42 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 // import { useRef } from 'react';
 
 function App() {
-  function getTime(){
-    return new Date().toLocaleTimeString()
+  function getTime() {
+    return new Date().toLocaleTimeString();
   }
   const [time, setTime] = useState(getTime());
   setInterval(() => {
-    setTime(getTime ());
+    setTime(getTime());
   }, 1000);
   return (
     <>
-    <div className='clockFrame'>
       <div
-        className="Clock"
+        className="clockFrame"
         style={{
-          height: "30vh",
-          width: "30vw",
-          backgroundColor: "black",
-          color: "white",
-          fontSize:"10px",
-          display:"flex",
-          alignItems:"center",
-          justifyContent:"center",
-          fontSize:"40px",
-          border:"3px solid gray",
-          borderRadius:"10px"
+          height: "65vh",
+          width: "35vw",
+          backgroundColor: "#00060A",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "3px solid gray",
+          borderRadius: "50%",
+          boxShadow:""
         }}
       >
-       {time}
-      </div>
+        <div
+          className="Clock"
+          style={{
+            height: "55vh",
+            width: "30vw",
+            backgroundColor: "black",
+            color: "white",
+            fontSize: "10px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "67px",
+            border: "3px solid #A1BE00",
+            borderRadius: "50%",
+          }}
+        >
+          {time}
+        </div>
       </div>
     </>
   );
 }
 
-
-export default App
+export default App;
