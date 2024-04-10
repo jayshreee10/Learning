5ecf8de41f3582ee67adc170a5e70b481458be8c
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-04-10 21:30:21 +0530

    testing Api for Tempstation

--- React/TestWeatherApi/src/App.jsx
+++ React/TestWeatherApi/src/App.jsx
@@ -1,22 +1,44 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [data, setdata] = useState(
-  //   temperature = 50,
-  //   location = "odisha",
-  //   humidity =  10 ,
-  //   wind = 20 ,
-  //   pressure = 60
 
-  // );
-  // useEffect ( ()=>{
-     const apiUrlForCity = "http://api.openweathermap.org/geo/1.0/direct?q=berhampur&appid=7d3a506953bf739eb497cd7257dfe861"
-    //  data = apiUrlForCity.name 
-     console.log (apiUrlForCity. name )
-      // setdata ()=>{data, location; apiUrlForCity.name }
-  // },[])
-  
+  let defaultData = {
+    temperature : "",
+    location : "",
+    humidity :  "" ,
+    wind : "" ,
+    pressure : ""
+
+  }
+   const  [data,setData]= useState(defaultData)
+    async function callApi (){
+    const response = await fetch ("http://api.openweathermap.org/geo/1.0/direct?q=berhampur&appid=7d3a506953bf739eb497cd7257dfe861")
+    const apiData = await response.json()
+    console.log (apiData )
+    let longitude = apiData[0].lon;
+    let latitude = apiData[0].lat;
+    console.log (longitude)
+    console.log (latitude)
+    const getLocation = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7d3a506953bf739eb497cd7257dfe861`)
+    const locationData = await getLocation.json()
+    console.log (locationData)
+    let newData = {
+      temperature :locationData.main.temp ,
+      location : locationData.name,
+      humidity : locationData.main.humidity  ,
+      wind :locationData.wind.speed,
+      pressure : locationData.main.pressure
+    }
+    console.log(newData.temperature)
+    console.log(newData.location)
+    console.log(newData.humidity)
+    console.log(newData. wind)
+    console.log(newData.pressure)
+    setData(locationData)
+    
+  }
 
   return (
     <>
@@ -25,38 +47,38 @@ function App() {
         style={{ height: "10vh", width: "100vw", backgroundColor: "black", }}
       >
         <input type="text" placeholder="location" style={{height:"4vh",width:"40vw",marginTop:"10px"}}/>
-        <button type="button">search</button>
+        <button type="button" onClick={()=>{callApi()}}>search</button>
       </div>
 
       <div
         className="temperature"
         style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
       >
-        rfg
+       {newData.temperature}
       </div>
       <div
         className="location"
         style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
       >
-       ff 
+      {newData.location}
       </div>
       <div
         className="humidity"
         style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
       >
-       gsx
+       {newData.humidity}
       </div>
       <div
         className="pressure"
         style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
       >
-        sxbg
+        {newData.pressure}
       </div>
       <div
         className="wind"
         style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
       >
-    sgb
+    {newData.wind}
       </div>
     </>
   );
