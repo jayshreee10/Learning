0703b8a6177a7b492544bd2c2d83057e69fd1aef
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-04 21:24:08 +0530

    problem solving

--- Java/concepts/Number Traverse/TwoIpArmStrong.java
+++ Java/concepts/Number Traverse/TwoIpArmStrong.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class TwoIpArmStrong {
     public static void main (String[]args){
-        int start,stop, sum,num;
+        int start,stop,num;
         System.out.println("Enter the starting number");
         Scanner sc = new Scanner( System.in);
         start = sc.nextInt();
@@ -11,13 +11,16 @@ public class TwoIpArmStrong {
 
         for ( int i = start ; i <= stop ; i++ ){
 
-            num = i ; sum = 0;
+            num = i ; int sum=0 ;
             while (num != 0){
-              int  rmd = num %10;
+             int rmd = num%10;
                 num = num/10;
                 sum = sum + (rmd*rmd* rmd);
+                // System.out.println(sum + "sum");
+
+
             }
-            if (sum == num){
+            if (sum == i ){
                 System.out.println(i + " is a Armstrong Number");
             }
             else{
