b71ef461df4882d8ff482f7dcd8d3561d512a891
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-16 20:30:10 +0530

    API in depth

--- React/axios/src/ApiCall.jsx
+++ React/axios/src/ApiCall.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import TodoItem from "./TodoItem";
+import axios from "axios";
 function ApiCall() {
   const [todos, setTodos] = useState([]);
   const Base_url = "https://jsonplaceholder.typicode.com/todos/";
+
   async function callApi() {
     try {
       const response = await fetch(Base_url);
@@ -16,11 +18,43 @@ function ApiCall() {
     }
   }
 
+  async function axiosCall() {}
+  const [sendUserid, setSendUserid] = useState("");
+
+  async function SearchUserId() {
+    try {
+      let new_url = Base_url + "?userId=" + sendUserid;
+      const response = await axios.get(new_url);
+
+      if (response.status == 200) {
+        let data = response.data;
+        console.log(data);
+        setTodos(data);
+      }
+    } catch (error) {
+      console.log("Something went wrong");
+    }
+  }
   return (
     <div>
+      <div>
+        <input
+          style={{
+            background: "white",
+            border: "2px solid navy",
+            margin: "10px",
+            height: "50px",
+            color: "black",
+          }}
+          type="search"
+          onChange={(e) => {
+            setSendUserid(e.target.value);
+          }}
+        />
+      </div>
       <button
         onClick={() => {
-          callApi();
+          SearchUserId();
         }}
       >
         search
