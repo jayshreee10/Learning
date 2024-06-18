11fdda3e37924d03e118031be2870177c055db42
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-18 12:20:48 +0530

    Home page

---
+++ React/MuiDrawer/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from "react"
+// import PaymentHistory from "./page/PaymentHistory"
+import Home from "./page/Home.jsx"
+
+function App() {
+ 
+  return (
+    <>
+    <Home></Home>
+     {/* <PaymentHistory></PaymentHistory> */}
+    </>
+  )
+}
+
+export default App
